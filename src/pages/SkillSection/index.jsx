083c78132ccfde772dde816238686ac74de01f7b
Sarah Wettengeel <sarahwettengel@gmx.de>
2023-05-22 17:18:59 +0200

    skills and about page

---
+++ src/pages/SkillSection/index.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../components/Footer/Footer";
+import Navbar from "../../components/Nav/Navigation";
+import { Skills } from "../../components/Skills/Skills";
+
+export default function SkillSection() {
+  return (
+    <>
+      <Navbar />
+      <Skills />
+      <Footer />
+    </>
+  );
+}
